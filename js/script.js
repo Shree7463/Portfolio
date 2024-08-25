5c483ff6920a91d5c3bf82f1bd00abb10bdd4c38
Shree7463 <96698802+Shree7463@users.noreply.github.com>
2024-08-25 20:44:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=====================typing animation=========*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Java Developer","SQL Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
